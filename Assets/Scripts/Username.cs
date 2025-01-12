1c0a8c34c26574b071565d62657434daf3c30997
1 <jamesjfy@163.com>
2025-01-12 18:03:13 +0800

    new filenames

--- Assets/Scripts/Username.cs
+++ Assets/Scripts/Username.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Username : MonoBehaviour
+namespace Register
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Username : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
